2572788d3eef1f6a091b2365dbc397adf9dc4f93
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2025-07-07 14:38:14 +0800

    build: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const ProviderScope(child: App()));
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+        body: Center(child: const Text('Hello World')),
+      ),
+    );
+  }
+}
